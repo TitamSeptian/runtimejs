622188eda7a0877db4cb2af3203d981557b56cfb
TitamSeptian <dwiputra4456@protonmail.com>
2024-08-08 21:26:57 +0700

    update response

--- bunN/app.js
+++ bunN/app.js
@@ -1,41 +1,11 @@
 Bun.serve({
   port: 8001,
-  hostname: '192.168.100.183',
+  hostname: '192.168.1.11',
   fetch(req) {
-    const data = {
-      method: req.method,
-      url: req.url,
-      headers: req.headers,
-      message: 'Hello World!',
-    }
-    console.log(data)
-    // method get
-    if (req.method === 'GET') {
-      return new Response(JSON.stringify(data), {
-        headers: { 'Content-Type': 'application/json' },
-      });
-    }
-
-    // method post
-    if (req.method === 'POST') {
-      return new Response(JSON.stringify(data), {
-        headers: { 'Content-Type': 'application/json' },
-      });
-    }
-
-    // method put
-    if (req.method === 'PUT') {
-      return new Response(JSON.stringify(data), {
-        headers: { 'Content-Type': 'application/json' },
-      });
-    }
-
-    // method delete
-    if (req.method === 'DELETE') {
-      return new Response(JSON.stringify(data), {
-        headers: { 'Content-Type': 'application/json' },
-      });
-    }
-
+    const body = { message: 'Hello World' };
+    console.log(body);
+    return new Response(JSON.stringify(body), {
+      headers: { 'Content-Type': 'application/json' },
+    });
   },
 });
--- deno/app.js
+++ deno/app.js
@@ -1,48 +1,14 @@
 const port = 8001;
-const   hostname = '192.168.100.183';
-const handler = (request) => {
-  const body = JSON.stringify({
-    method: request.method,
-    url: request.url,
-    headers: {
-      host: request.headers.get('host'),
-      'user-agent': request.headers.get('user-agent'),
-      'content-type': request.headers.get('content-type'),
-      connection: request.headers.get('connection'),
-      'accept-encoding': request.headers.get('accept-encoding'),
-    },
-    message: 'Hello World!',
-  });
-
-  console.log(body);
-
-  if (request.method === 'GET') {
-    return new Response(body, {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-
-  if (request.method === 'POST') {
-    return new Response(body, {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-
-  if (request.method === 'PUT') {
-    return new Response(body, {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-
-  if (request.method === 'DELETE') {
-    return new Response(body, {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-  
+const hostname = '192.168.1.11';
+const body = {
+  message: 'Hello World',
 };
-
-console.log(`HTTP server running. Access it at: http://${hostname}:${port}/`);
+const handler = (request) => {
+  console.log(body);
+  return new Response(JSON.stringify(body), {
+    headers: { 'Content-Type': 'application/json' },
+  });
+};
 // to allow on network 
 // $ deno run --allow-net .\app.js
 Deno.serve({ port }, handler);
